756b92d29e22a4279c140ab11dbbb155443280e5
Caio Macedo <53654561+LordeNCaio@users.noreply.github.com>
2022-05-12 10:28:15 -0300

    Commit Inicial

---
+++ src/main/java/br/com/study/listmanagerback/ListManagerBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.study.listmanagerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListManagerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListManagerBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/study/listmanagerback/ListManagerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study.listmanagerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListManagerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
